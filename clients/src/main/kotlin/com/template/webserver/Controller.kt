c500328626b246ec9d11b365a87b8430d3fc1756
simsonraj <simsonraj@traxiontech.net>
2019-09-20 15:29:14 +0800

    changes for the http api

--- clients/src/main/kotlin/com/template/webserver/Controller.kt
+++ clients/src/main/kotlin/com/template/webserver/Controller.kt
@@ -1,9 +1,27 @@
 package com.template.webserver
 
+
+
+import net.corda.core.messaging.startTrackedFlow
 import org.slf4j.LoggerFactory
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.MediaType.TEXT_PLAIN_VALUE
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import javax.servlet.http.HttpServletRequest
+
+import com.template.flows.ExampleFlowWithEvolvableToken
+import com.template.states.ExampleEvolvableTokenType
+import net.corda.core.contracts.LinearState
+import net.corda.core.contracts.StateAndRef
+import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.messaging.vaultQueryBy
+import net.corda.core.node.services.Vault
+import net.corda.core.node.services.vault.*
+import net.corda.core.utilities.getOrThrow
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 
 /**
  * Define your API endpoints here.
@@ -22,4 +40,73 @@ class Controller(rpc: NodeRPCConnection) {
     private fun templateendpoint(): String {
         return "Define an endpoint here."
     }
+
+
+
+    @PostMapping(value = [ "create-tokens" ], produces = [ TEXT_PLAIN_VALUE ], headers = [ "Content-Type=application/x-www-form-urlencoded" ])
+    fun createTokens(request: HttpServletRequest): ResponseEntity<String> {
+        val data = request.getParameter("informationThatMayChange").trim()
+        //val date = LocalDate.parse(request.getParameter("date").substringBefore("00").trim(), DateTimeFormatter.ofPattern("E MMMM d yyyy"))
+        //val partyName = request.getParameter("megacorp") ?: return ResponseEntity.badRequest().body("Query parameter 'MegaCorp' must not be null.\n")
+       /* if (hoursWorked <= 0 ) {
+            return ResponseEntity.badRequest().body("Query parameter 'hoursWorked' must be non-negative.\n")
+        }
+        val partyX500Name = CordaX500Name.parse(partyName)
+        val otherParty = proxy.wellKnownPartyFromX500Name(partyX500Name) ?: return ResponseEntity.badRequest().body("Party named $partyName cannot be found.\n")
+*/
+        return try {
+            val signedTx = proxy.startTrackedFlow(ExampleFlowWithEvolvableToken::CreateExampleEvolvableToken, data).returnValue.getOrThrow()
+            ResponseEntity.status(HttpStatus.CREATED).body("Transaction id ${signedTx.id} committed to ledger.\n")
+        } catch (ex: Throwable) {
+            logger.error(ex.message, ex)
+            ResponseEntity.badRequest().body(ex.message!!)
+        }
+    }
+/*
+    @GetMapping(value = "states", produces = [ TEXT_PLAIN_VALUE ])
+    private fun states(): ResponseEntity<String>  {
+        val criteria = QueryCriteria.VaultQueryCriteria(status = Vault.StateStatus.ALL)
+        val paging = PageSpecification(1, 10)
+        val sorting = Sort(setOf(Sort.SortColumn(SortAttribute.Standard(Sort.VaultStateAttribute.RECORDED_TIME), Sort.Direction.DESC)))
+        return ResponseEntity.status(HttpStatus.CREATED).body(proxy.vaultQueryBy<ExampleEvolvableTokenType>(criteria,paging,sorting).states.toString())
+    }*/
+
+    @GetMapping(value = [ "states" ], produces = [ APPLICATION_JSON_VALUE ])
+    fun getMyIOUs(): ResponseEntity<List<UniqueIdentifier>>? {
+       //val myious = proxy.vaultQueryBy<ExampleEvolvableTokenType>().states.filter { it.state.data.maintainer.equals(proxy.nodeInfo().legalIdentities.first()) }
+        val myious = proxy.vaultQueryBy<ExampleEvolvableTokenType>().states.filter { it.state.data.maintainer.equals(proxy.nodeInfo().legalIdentities.first()) }
+        val linearIds = myious.map { it.state.data.linearId }.toList()
+        logger.error("responsee haiii"+linearIds.toString())
+        val criteria = QueryCriteria.VaultQueryCriteria(status = Vault.StateStatus.ALL)
+        val results = proxy.vaultQueryBy<LinearState>(criteria).states
+       return ResponseEntity.ok(linearIds)
+    }
+
+    /**
+     * Displays all IOU states that exist in the node's vault.
+     */
+  /*  @GetMapping(value = [ "invoices" ], produces = [ APPLICATION_JSON_VALUE ])
+    fun getInvoices() : ResponseEntity<List<StateAndRef<InvoiceState>>> {
+        return ResponseEntity.ok(proxy.vaultQueryBy<InvoiceState>().states)
+    }*/
+/*
+    @PostMapping(value = [ "Issue-tokens" ], produces = [ TEXT_PLAIN_VALUE ], headers = [ "Content-Type=application/x-www-form-urlencoded" ])
+    fun issueTokens(request: HttpServletRequest): ResponseEntity<String> {
+        val data = request.getParameter("informationThatMayChange").trim()
+        //val date = LocalDate.parse(request.getParameter("date").substringBefore("00").trim(), DateTimeFormatter.ofPattern("E MMMM d yyyy"))
+        //val partyName = request.getParameter("megacorp") ?: return ResponseEntity.badRequest().body("Query parameter 'MegaCorp' must not be null.\n")
+        /* if (hoursWorked <= 0 ) {
+             return ResponseEntity.badRequest().body("Query parameter 'hoursWorked' must be non-negative.\n")
+         }
+         val partyX500Name = CordaX500Name.parse(partyName)
+         val otherParty = proxy.wellKnownPartyFromX500Name(partyX500Name) ?: return ResponseEntity.badRequest().body("Party named $partyName cannot be found.\n")
+ */
+        return try {
+            val signedTx = proxy.startTrackedFlow(ExampleFlowWithEvolvableToken::CreateExampleEvolvableToken, data).returnValue.getOrThrow()
+            ResponseEntity.status(HttpStatus.CREATED).body("Transaction id ${signedTx.id} committed to ledger.\n")
+        } catch (ex: Throwable) {
+            logger.error(ex.message, ex)
+            ResponseEntity.badRequest().body(ex.message!!)
+        }
+    }*/
 }
--- workflows/src/main/kotlin/com/template/flows/ExampleFlowWithEvolvableToken.kt
+++ workflows/src/main/kotlin/com/template/flows/ExampleFlowWithEvolvableToken.kt
@@ -24,82 +24,85 @@ import net.corda.core.flows.FlowException
 import net.corda.core.transactions.SignedTransaction
 import net.corda.core.identity.Party
 import net.corda.core.flows.FlowLogic
+import net.corda.core.flows.InitiatingFlow
 import net.corda.core.flows.StartableByRPC
 
 
 
 
+object ExampleFlowWithEvolvableToken {
+    @StartableByRPC
+    class IssueExampletoken(
+            val evolvableTokenId: String,
+            val amount: Long,
+            val recipient: Party
+    ) : FlowLogic<SignedTransaction>() {
+        override val progressTracker = ProgressTracker()
 
-@StartableByRPC
-class ExampleFlowWithEvolvableToken(
-        val evolvableTokenId: String,
-        val amount: Long,
-        val recipient: Party
-) : FlowLogic<SignedTransaction>() {
-    override val progressTracker = ProgressTracker()
-
-    @Suspendable
-    override fun call(): SignedTransaction {
-        val notary = serviceHub.networkMapCache.notaryIdentities.first()
-        val uuid = UUID.fromString(evolvableTokenId)
-        val queryCriteria = QueryCriteria.LinearStateQueryCriteria(uuid = listOf(uuid))
-        val tokenStateAndRef = serviceHub.vaultService.queryBy<EvolvableTokenType>(queryCriteria).states.single()
-        val token = tokenStateAndRef.state.data.toPointer<EvolvableTokenType>()
-        // Starting this flow with a new flow session.
-        val issueTokensFlow = IssueTokens(listOf(amount of token issuedBy ourIdentity heldBy recipient))
-        return subFlow(issueTokensFlow)
+        @Suspendable
+        override fun call(): SignedTransaction {
+            val notary = serviceHub.networkMapCache.notaryIdentities.first()
+            val uuid = UUID.fromString(evolvableTokenId)
+            val queryCriteria = QueryCriteria.LinearStateQueryCriteria(uuid = listOf(uuid))
+            val tokenStateAndRef = serviceHub.vaultService.queryBy<EvolvableTokenType>(queryCriteria).states.single()
+            val token = tokenStateAndRef.state.data.toPointer<EvolvableTokenType>()
+            // Starting this flow with a new flow session.
+            val issueTokensFlow = IssueTokens(listOf(amount of token issuedBy ourIdentity heldBy recipient))
+            return subFlow(issueTokensFlow)
+        }
     }
-}
 
-@StartableByRPC
-class CreateExampleEvolvableToken(val data: String) : FlowLogic<SignedTransaction>() {
-    override val progressTracker = ProgressTracker()
+    @InitiatingFlow
+    @StartableByRPC
+    class CreateExampleEvolvableToken(val data: String) : FlowLogic<SignedTransaction>() {
+        override val progressTracker = ProgressTracker()
 
-    @Suspendable
-    override fun call(): SignedTransaction {
-        val notary = serviceHub.networkMapCache.notaryIdentities.first()
-        val evolvableTokenType = ExampleEvolvableTokenType(data, ourIdentity, linearId = UniqueIdentifier())
-        val transactionState = TransactionState(evolvableTokenType, notary = notary)
-        return subFlow(CreateEvolvableTokens(transactionState))
+        @Suspendable
+        override fun call(): SignedTransaction {
+            val notary = serviceHub.networkMapCache.notaryIdentities.first()
+            val evolvableTokenType = ExampleEvolvableTokenType(data, ourIdentity, linearId = UniqueIdentifier())
+            val transactionState = TransactionState(evolvableTokenType, notary = notary)
+            return subFlow(CreateEvolvableTokens(transactionState))
+        }
     }
-}
 
-@StartableByRPC
-class MoveEvolvableFungibleTokenFlow(private val tokenId: String, private val holder: Party, private val quantity: Int) : FlowLogic<SignedTransaction>() {
+    @StartableByRPC
+    class MoveEvolvableFungibleTokenFlow(private val tokenId: String, private val holder: Party, private val quantity: Int) : FlowLogic<SignedTransaction>() {
 
-    @Suspendable
-    @Throws(FlowException::class)
-    override fun call(): SignedTransaction {
-        //get uuid from input tokenId
-        val uuid = UUID.fromString(tokenId)
-        //create criteria to get all unconsumed house states on ledger with uuid as input tokenId
-        val queryCriteria = QueryCriteria.LinearStateQueryCriteria(null, ImmutableList.of(uuid), null,
-                Vault.StateStatus.UNCONSUMED, null)
-        val tokenStateAndRef = serviceHub.vaultService.queryBy<EvolvableTokenType>(queryCriteria).states.single()
-        val token = tokenStateAndRef.state.data.toPointer<EvolvableTokenType>()
-        //specify how much amount to transfer to which holder
-        val amount = Amount(quantity.toLong(), token)
-        val partyAndAmount = PartyAndAmount(holder, amount)
-        // Starting this flow with a new flow session.
-        val moveTokensFlow = MoveFungibleTokens(quantity.toLong() of token, holder)
-        return subFlow(moveTokensFlow)
+        @Suspendable
+        @Throws(FlowException::class)
+        override fun call(): SignedTransaction {
+            //get uuid from input tokenId
+            val uuid = UUID.fromString(tokenId)
+            //create criteria to get all unconsumed house states on ledger with uuid as input tokenId
+            val queryCriteria = QueryCriteria.LinearStateQueryCriteria(null, ImmutableList.of(uuid), null,
+                    Vault.StateStatus.UNCONSUMED, null)
+            val tokenStateAndRef = serviceHub.vaultService.queryBy<EvolvableTokenType>(queryCriteria).states.single()
+            val token = tokenStateAndRef.state.data.toPointer<EvolvableTokenType>()
+            //specify how much amount to transfer to which holder
+            val amount = Amount(quantity.toLong(), token)
+            val partyAndAmount = PartyAndAmount(holder, amount)
+            // Starting this flow with a new flow session.
+            val moveTokensFlow = MoveFungibleTokens(quantity.toLong() of token, holder)
+            return subFlow(moveTokensFlow)
+        }
     }
-}
 
-@StartableByRPC
-class RedeemHouseFungibleTokenFlow(private val tokenId: String, private val issuer: Party, private val quantity: Int) : FlowLogic<SignedTransaction>() {
+    @StartableByRPC
+    class RedeemHouseFungibleTokenFlow(private val tokenId: String, private val issuer: Party, private val quantity: Int) : FlowLogic<SignedTransaction>() {
 
-    @Suspendable
-    @Throws(FlowException::class)
-    override fun call(): SignedTransaction {
-        //get uuid from input tokenId
-        val uuid = UUID.fromString(tokenId)
-        //create criteria to get all unconsumed house states on ledger with uuid as input tokenId
-        val queryCriteria = QueryCriteria.LinearStateQueryCriteria(null, ImmutableList.of(uuid), null,
-                Vault.StateStatus.UNCONSUMED, null)
-        val tokenStateAndRef = serviceHub.vaultService.queryBy<EvolvableTokenType>(queryCriteria).states.single()
-        val token = tokenStateAndRef.state.data.toPointer<EvolvableTokenType>()
-        val amount = Amount(quantity.toLong(), token)
-        return subFlow(RedeemFungibleTokens(quantity.toLong() of token, issuer))
+        @Suspendable
+        @Throws(FlowException::class)
+        override fun call(): SignedTransaction {
+            //get uuid from input tokenId
+            val uuid = UUID.fromString(tokenId)
+            //create criteria to get all unconsumed house states on ledger with uuid as input tokenId
+            val queryCriteria = QueryCriteria.LinearStateQueryCriteria(null, ImmutableList.of(uuid), null,
+                    Vault.StateStatus.UNCONSUMED, null)
+            val tokenStateAndRef = serviceHub.vaultService.queryBy<EvolvableTokenType>(queryCriteria).states.single()
+            val token = tokenStateAndRef.state.data.toPointer<EvolvableTokenType>()
+            val amount = Amount(quantity.toLong(), token)
+            return subFlow(RedeemFungibleTokens(quantity.toLong() of token, issuer))
+        }
     }
 }
